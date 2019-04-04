b8122d80b34bcb3f4eada15412d0b2f126190424
Kelly Innes <kinnes@azavea.com>
2019-04-04 12:17:38 -0400

    Remove unused BundleAnalyzer plugin from staging webpack file

--- webpack.staging.config.js
+++ webpack.staging.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: {
